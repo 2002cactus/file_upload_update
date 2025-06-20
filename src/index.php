8819d21777bb861bf30cdee5c3e3e34a3e753871
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-06-20 16:24:21 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -50,3 +50,42 @@ if (isset($_FILES["file"])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>PHP upload Level 2</title>
+
+    <!-- This is for UI only -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body>
+    <br />
+    <br />
+    <h3 class="display-4 text-center">File upload workshop</h3>
+    <h4 class="display-4 text-center">Level 2</h4>
+    <p class="display-5 text-center">Goal: RCE me!</p>
+
+    <br />
+    <div class="container">
+        <a href="/?debug">Debug source</a>
+        <form method="post" enctype="multipart/form-data">
+            Select file to upload:
+            <input type="file" name="file" id="file">
+            <br />
+            <input type="submit">
+        </form>
+        <span style="color:red"><?php echo $error; ?></span>
+        <span style="color:green"><?php echo $success; ?></span>
+    </div>
+
+</body>
+
+</html>
